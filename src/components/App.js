e1577fedb287415ff16956fcba415c9fa3c3c28b
Varun Kumar <heysadboy@gmail.com>
2022-10-06 18:05:39 +0200

    Displaying CSV file data
    Displaying data in the form of table

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,62 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { csv } from "d3-fetch";
 import file from "../data/file.csv";
 
 const App = () => {
-  const getData = async () => {
-    const data = await csv(file);
-    console.log(data);
+  const [data, setData] = useState([]);
+  const [columns, setColumns] = useState([]);
+
+  const getRowKey = (item) => {
+    return `${item.origin}-${item.destination}-${item.short_name}`;
+  };
+
+  const getHeaders = () => {
+    return (
+      <tr>
+        {columns.map((title) => {
+          return <th key={title}>{title}</th>;
+        })}
+      </tr>
+    );
+  };
+
+  const getRows = () => {
+    return data.map((item) => {
+      if (typeof item === "object") {
+        const key = getRowKey(item);
+        return (
+          <tr key={key}>
+            {columns.map((row) => {
+              return (
+                <td key={`${row}-${key}`} data-label={row}>
+                  {item[`${row}`]}
+                </td>
+              );
+            })}
+          </tr>
+        );
+      }
+      return false;
+    });
   };
 
   useEffect(() => {
-    getData();
+    const getCsvData = async () => {
+      const csvData = await csv(file);
+      setData(csvData);
+      setColumns(csvData.columns);
+    };
+    getCsvData();
   }, []);
 
-  return <div>Hi</div>;
+  return (
+    <div>
+      <table className="ui celled table">
+        <thead>{getHeaders()}</thead>
+        <tbody>{getRows()}</tbody>
+      </table>
+    </div>
+  );
 };
 
 export default App;
