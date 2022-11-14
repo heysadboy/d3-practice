f9dab984dfcb43ec89b039022666356441dbcfd1
Varun Kumar <heysadboy@gmail.com>
2022-11-14 09:54:29 +0100

    Updating donut chart

--- src/components/Donut.jsx
+++ src/components/Donut.jsx
@@ -3,20 +3,24 @@ import * as d3 from "d3";
 import "../style.css";
 
 const Donut = () => {
-  const arcPathGenerator = d3
-    .arc()
-    .innerRadius(300)
-    .outerRadius(340)
-    .startAngle(0)
-    .endAngle(5.5)
-    .padAngle(0)
-    .cornerRadius(20);
+  const arcPathGenerator = (start, end) => {
+    return d3
+      .arc()
+      .innerRadius(100)
+      .outerRadius(140)
+      .startAngle(start * 2)
+      .endAngle(end * 2);
+  };
 
-  const arcPath = arcPathGenerator();
+  const arcPath1 = arcPathGenerator(0 * Math.PI, 0.25 * Math.PI)();
+  const arcPath2 = arcPathGenerator(0.25 * Math.PI, 0.85 * Math.PI)();
+  const arcPath3 = arcPathGenerator(0.85 * Math.PI, 1 * Math.PI)();
 
   return (
     <svg width="800" height="800">
-      <path fill="cornflowerblue" d={arcPath} className="donutPath" />
+      <path fill="blue" d={arcPath1} className="donutPath" />
+      <path fill="red" d={arcPath2} className="donutPath" />
+      <path fill="yellow" d={arcPath3} className="donutPath" />
     </svg>
   );
 };
